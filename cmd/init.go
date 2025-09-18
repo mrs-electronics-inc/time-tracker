f20e387efe4d2c84cebbfa2247407681cbfe1c5b
Addison Emig <addison.emig@mrs-electronics.com>
2025-09-17 20:27:47 -0400

    feat: automatically create tasks file (#8)

--- cmd/init.go
+++ cmd/init.go
@@ -9,6 +9,8 @@ import (
 	"os"
 
 	"github.com/LeanMendez/time-tracker/config"
+	"github.com/LeanMendez/time-tracker/models"
+	"github.com/LeanMendez/time-tracker/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -21,14 +23,15 @@ var initCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		path := args[0]
 
+		// Create the storage path
 		if err := os.MkdirAll(path, 0755); err != nil {
 			return fmt.Errorf("failed to create directory: %w", err)
 		}
 
+		// Save the storage path to the configuration file
 		configLocal := config.Config{
 			StoragePath: path,
 		}
-
 		configData, err := json.Marshal(configLocal)
 		if err != nil {
 			return fmt.Errorf("failed to marshal config: %w", err)
@@ -36,6 +39,15 @@ var initCmd = &cobra.Command{
 
 		if err := os.WriteFile(config.ConfigFile, configData, 0644); err != nil {
 			return fmt.Errorf("failed to write config file: %w", err)
+		}
+
+		// This will initialize an empty tasks.json
+		taskManager, err := utils.NewTaskManager(config.ConfigFile)
+		if err != nil {
+			return fmt.Errorf("failed to create tasks.json: %w", err)
+		}
+		if err := taskManager.SaveTasks([]models.Task{}); err != nil {
+			return fmt.Errorf("failed to create tasks.json: %w", err)
 		}
 
 		fmt.Printf("Initialized time-tracker with storage path: %s \n", path)

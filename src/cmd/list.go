4c1e135a74930b4b1f76717111fc3b528548aede
Addison <addison.emig@mrs-electronics.com>
2025-09-30 15:12:35 -0400

    docs: one more word

--- src/cmd/list.go
+++ src/cmd/list.go
@@ -17,7 +17,7 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List time entries",
 	Long: `List time entries from data.json in chronological order (newest first).
-By default, only entries from the current day will be shown. Use --all to view all entries`,
+By default, only the entries from the current day will be shown. Use --all to view all entries`,
 	Aliases: []string{"l", "ls"},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		displayAll, err := cmd.Flags().GetBool("all")

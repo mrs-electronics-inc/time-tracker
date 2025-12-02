be4ba0df0c633390f6e3863e7633a9a9aee7e714
Addison Emig <addison.emig@mrs-electronics.com>
2025-12-02 15:48:53 -0500

    feat: launch TUI by default when no subcommand provided

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
+	"time-tracker/cmd/tui"
+	"time-tracker/config"
+	"time-tracker/utils"
 )
 
 var rootCmd = &cobra.Command{
@@ -11,6 +16,26 @@ var rootCmd = &cobra.Command{
 	Short: "A simple time tracker",
 	Long: `A simple time tracker TUI application.
 See https://github.com/mrs-electronics-inc/time-tracker for more details.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// If no subcommand provided, launch the TUI
+		if len(args) == 0 {
+			storage, err := utils.NewFileStorage(config.DataFilePath())
+			if err != nil {
+				return fmt.Errorf("failed to initialize storage: %w", err)
+			}
+			taskManager := utils.NewTaskManager(storage)
+
+			model := tui.NewModel(storage, taskManager)
+			if err := model.LoadEntries(); err != nil {
+				return fmt.Errorf("failed to load entries: %w", err)
+			}
+
+			p := tea.NewProgram(model)
+			_, err = p.Run()
+			return err
+		}
+		return cmd.Help()
+	},
 }
 
 func Execute() {

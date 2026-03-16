c0399c31b8fff7987707b91a935bddfb25c454a3
Addison Emig <17209828+Pertempto@users.noreply.github.com>
2026-03-16 12:47:36 -0400

    feat: project domain logic (#54)

--- utils/task_manager.go
+++ utils/task_manager.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"sort"
+	"strings"
 	"time"
 
 	"time-tracker/models"
@@ -18,6 +19,20 @@ type Storage interface {
 
 type TaskManager struct {
 	storage Storage
+}
+
+type ProjectMutationResult struct {
+	RewrittenEntries int
+	Merged           bool
+}
+
+type ProjectInUseError struct {
+	ProjectName    string
+	ReferenceCount int
+}
+
+func (e *ProjectInUseError) Error() string {
+	return fmt.Sprintf("cannot remove project %q: referenced by %d time entries", e.ProjectName, e.ReferenceCount)
 }
 
 func NewTaskManager(storage Storage) *TaskManager {
@@ -154,4 +169,178 @@ func (tm *TaskManager) DeleteEntry(idx int) error {
 	}
 
 	return tm.storage.Save(entries)
+}
+
+func (tm *TaskManager) AddProject(name, code, category string) (*models.Project, error) {
+	name = strings.TrimSpace(name)
+	code = strings.TrimSpace(code)
+	category = strings.TrimSpace(category)
+
+	if name == "" {
+		return nil, fmt.Errorf("project name cannot be empty")
+	}
+
+	projects, err := tm.storage.LoadProjects()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, project := range projects {
+		if strings.EqualFold(project.Name, name) {
+			return nil, fmt.Errorf("project %q already exists", name)
+		}
+	}
+
+	newProject := models.Project{Name: name, Code: code, Category: category}
+	projects = append(projects, newProject)
+
+	if err := tm.storage.SaveProjects(projects); err != nil {
+		return nil, err
+	}
+
+	return &newProject, nil
+}
+
+func (tm *TaskManager) EditProject(name, newName, code, category string) (*ProjectMutationResult, error) {
+	name = strings.TrimSpace(name)
+	newName = strings.TrimSpace(newName)
+	code = strings.TrimSpace(code)
+	category = strings.TrimSpace(category)
+
+	if name == "" {
+		return nil, fmt.Errorf("project name cannot be empty")
+	}
+
+	projects, err := tm.storage.LoadProjects()
+	if err != nil {
+		return nil, err
+	}
+
+	sourceIndex := -1
+	for i, project := range projects {
+		if strings.EqualFold(project.Name, name) {
+			sourceIndex = i
+			break
+		}
+	}
+	if sourceIndex < 0 {
+		return nil, fmt.Errorf("project %q not found", name)
+	}
+
+	source := projects[sourceIndex]
+	if newName == "" {
+		newName = source.Name
+	}
+
+	if newName == "" {
+		return nil, fmt.Errorf("project name cannot be empty")
+	}
+
+	targetIndex := -1
+	for i, project := range projects {
+		if i != sourceIndex && strings.EqualFold(project.Name, newName) {
+			targetIndex = i
+			break
+		}
+	}
+
+	entries, err := tm.storage.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	rewrittenEntries := 0
+	if targetIndex >= 0 {
+		targetName := projects[targetIndex].Name
+		for i := range entries {
+			if entries[i].Project == source.Name {
+				entries[i].Project = targetName
+				rewrittenEntries++
+			}
+		}
+
+		projects = append(projects[:sourceIndex], projects[sourceIndex+1:]...)
+		if err := tm.storage.Save(entries); err != nil {
+			return nil, err
+		}
+		if err := tm.storage.SaveProjects(projects); err != nil {
+			return nil, err
+		}
+
+		return &ProjectMutationResult{RewrittenEntries: rewrittenEntries, Merged: true}, nil
+	}
+
+	renamed := source.Name != newName
+	if renamed {
+		for i := range entries {
+			if entries[i].Project == source.Name {
+				entries[i].Project = newName
+				rewrittenEntries++
+			}
+		}
+	}
+
+	source.Name = newName
+	source.Code = code
+	source.Category = category
+	projects[sourceIndex] = source
+
+	if renamed {
+		if err := tm.storage.Save(entries); err != nil {
+			return nil, err
+		}
+		if err := tm.storage.SaveProjects(projects); err != nil {
+			return nil, err
+		}
+	} else {
+		if err := tm.storage.SaveProjects(projects); err != nil {
+			return nil, err
+		}
+	}
+
+	return &ProjectMutationResult{RewrittenEntries: rewrittenEntries, Merged: false}, nil
+}
+
+func (tm *TaskManager) RemoveProject(name string) error {
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return fmt.Errorf("project name cannot be empty")
+	}
+
+	projects, err := tm.storage.LoadProjects()
+	if err != nil {
+		return err
+	}
+
+	projectIndex := -1
+	projectName := ""
+	for i, project := range projects {
+		if strings.EqualFold(project.Name, name) {
+			projectIndex = i
+			projectName = project.Name
+			break
+		}
+	}
+	if projectIndex < 0 {
+		return fmt.Errorf("project %q not found", name)
+	}
+
+	entries, err := tm.storage.Load()
+	if err != nil {
+		return err
+	}
+
+	referenceCount := 0
+	for _, entry := range entries {
+		if entry.Project == projectName {
+			referenceCount++
+		}
+	}
+
+	if referenceCount > 0 {
+		return &ProjectInUseError{ProjectName: projectName, ReferenceCount: referenceCount}
+	}
+
+	projects = append(projects[:projectIndex], projects[projectIndex+1:]...)
+	return tm.storage.SaveProjects(projects)
 }
